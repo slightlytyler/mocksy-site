b09bd1ca4e0c274eae6f473f335d07431e35bba8
Tyler Martinez <slightlytyler@gmail.com>
2015-12-11 18:07:39 -0600

    Up and running

--- pages/_template.jsx
+++ pages/_template.jsx
@@ -1,25 +1,21 @@
 import React from 'react';
 import { RouteHandler, Link, State } from 'react-router';
-import { Container, Grid, Breakpoint, Span } from 'react-responsive-grid';
 
-import { rhythm, fontSizeToMS } from 'utils/typography';
+import Radium from 'radium';
 
-module.exports = React.createClass({
+
+const Index = React.createClass({
   mixins: [State],
 
   render: function() {
     return (
-      <div>
-        <Container
-          style={{
-            maxWidth: 960,
-            padding: `${rhythm(1)} ${rhythm(1/2)}`,
-            paddingTop: 0
-          }}
-        >
-          <RouteHandler {...this.props}/>
-        </Container>
-      </div>
+      <div style={styles.test}>Test</div>
     );
   }
 });
+
+const styles = {
+  test: { color: 'red' }
+};
+
+export default Radium(Index);
